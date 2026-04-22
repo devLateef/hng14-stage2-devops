86bcf8a12a9feb9014d79bb430e4b156bff5ba8c
devLateef <lateefolatayo@gmail.com>
2026-04-22 19:31:29 +0100

    ci: resolve some other issues

---
+++ frontend/eslint.config.js
@@ -0,0 +1,25 @@
+const js = require('@eslint/js');
+
+module.exports = [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'commonjs',
+      globals: {
+        require: 'readonly',
+        module: 'readonly',
+        exports: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        process: 'readonly',
+        console: 'readonly',
+      },
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+    },
+  },
+];
